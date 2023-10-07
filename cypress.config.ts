e1e3cfd0052b0c2dcc79bdad6b1444d6d4d750c9
Roberto-Parra <w.parrav@uniandes.edu.co>
2023-10-07 19:48:42 -0300

    cypress6

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,15 +1,6 @@
-import { defineConfig } from 'cypress';
-
-export default defineConfig({
-    video: false,
-    e2e: {
-        // We've imported your old cypress plugins here.
-        // You may want to clean this up later by importing these.
-        setupNodeEvents(on, config) {
-            return require('./cypress/plugins/index.ts')(
-            on,
-            config
-            );
-        },
-    },
-});
+// cypress.config.ts
+const config: Cypress.ConfigOptions = {
+    baseUrl: 'http://localhost:3000',
+    integrationFolder: 'cypress/integration'
+};
+export default config;
